c2010e3f1860821e227802f347f47f1c19da6f87
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2024-11-28 17:28:40 -0300

    Agregar relaciones para el Query Scope Include

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ class Category extends Model
 {
     use HasFactory, ApiTrait;
 
-    protected $allowIncluded = ['posts', 'posts.user'];
+    protected $allowIncluded = ['posts', 'posts.user', 'posts.images', 'posts.tags'];
     protected $allowFilter = ['id', 'name', 'slug'];
     protected $allowSort = ['id', 'name', 'slug'];
 
