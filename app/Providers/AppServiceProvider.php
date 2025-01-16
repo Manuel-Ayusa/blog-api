98ab7f50afdc724b1b0b8ed0e66c4de7ca551ac4
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-01-16 17:03:22 -0300

    Añadir y actualizar scopes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,16 +29,22 @@ class AppServiceProvider extends ServiceProvider
         Passport::tokensCan([
             'create-post' => 'Crear un nuevo post', 
             'read-post' => 'Leer un post', 
-            'update-post' => 'Actualizar un post', 
+            'update-post' => 'Actualizar un post',
             'delete-post' => 'Eliminar un post',
-            'create-category' => 'Crear uan nuevo categoria', 
-            'read-category' => 'Leer una categoria', 
-            'update-category' => 'Actualizar una categoria', 
-            'delete-category' => 'Eliminar una categoria'
+            'create-category' => 'Crear una nuevo categoria',
+            'read-category' => 'Leer una categoria',
+            'update-category' => 'Actualizar una categoria',
+            'delete-category' => 'Eliminar una categoria',
+            'create-tag' => 'Crear una nuevo etiqueta',
+            'read-tag' => 'Leer una etiqueta',
+            'update-tag' => 'Actualizar una etiqueta',
+            'delete-tag' => 'Eliminar una etiqueta'
         ]);
 
         Passport::setDefaultScope([
             'read-post',
+            'read-category',
+            'read-tag'
         ]);
     }
 }
