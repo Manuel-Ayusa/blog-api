7273c408933c819397b7612db0a7f52c1c8c6738
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-01-20 18:30:56 -0300

    Agregar rutas para CRUD de roles

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\RoleController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\UserController;
 
@@ -13,10 +14,12 @@ Route::post('login', [LoginController::class, 'store']);
 
 Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
 
+Route::resource('users', UserController::class)->only(['update'])->names('api.v1.users'); // Endpoints users
+
+Route::apiResource('roles', RoleController::class)->names('api.v1.roles'); // Endpoints roles
+
 Route::apiResource('categories', CategoryController::class)->names('api.v1.categories'); // Endpoints categorias
 
 Route::apiResource('posts', PostController::class)->names('api.v1.posts'); // Endpoints posts
 
 Route::apiResource('tags', TagController::class)->names('api.v1.tags'); // Endpoints tags
-
-Route::resource('users', UserController::class)->only(['update'])->names('api.v1.users'); // Endpoints users
