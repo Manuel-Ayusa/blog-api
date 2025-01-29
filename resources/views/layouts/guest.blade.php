4d81940feef1513450ca47b72a12e934b6eda239
Manuel-Ayusa <166891950+Manuel-Ayusa@users.noreply.github.com>
2025-01-29 17:50:07 -0300

    Cambiar estilos css

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon"  type="image/png" href="sa">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>API Blog</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -17,8 +18,8 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="http://api.codersfree.test/" class="font-bold text-xl font-mono bg-blue-300 p-2 rounded-2xl">
+                    APIBlog
                 </a>
             </div>
 
